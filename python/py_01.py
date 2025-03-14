03be537d77f78c237dd8881dc363b620d8c395bc
jeonghyun031 <pjh1849@gmail.com>
2025-03-14 20:20:05 +0900

    add python
    멀티미디어시스템 실습 과제

---
+++ python/py_01.py
@@ -0,0 +1,9 @@
+kor = 80
+eng = 100
+math = 73
+sc = 98
+
+print("%d + %d = %d" %(kor,eng,kor+eng))
+print("%d * %d = %d" %(eng,sc,eng*sc))
+print("%d + %d + %d = %d" %(eng,math,sc,eng+math+sc))
+print("%d - %d = %d" %(eng,kor,eng-kor))
---
+++ python/py_02.py
@@ -0,0 +1,5 @@
+r = 30 / 4
+pi = r * r * 3.14
+
+print(pi)
+print("<class 'float'>")
